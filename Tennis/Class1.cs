7d3633a3635ce0455de089eca015172c3b0d138d
Amy O'Connor <amy.oconnor@spektrix.com>
2023-11-15 10:37:34 +0000

    Extracted EndOfSetScore method

--- Tennis/Class1.cs
+++ Tennis/Class1.cs
@@ -31,11 +31,7 @@
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = EndOfSetScore();
             }
             else
             {
@@ -60,6 +56,17 @@
                     }
                 }
             }
+            return score;
+        }
+
+        private string EndOfSetScore()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
             return score;
         }
 
