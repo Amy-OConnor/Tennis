4a6b26ff087bdeb2bf6edbf0d6d0c2432c5a6bc8
robinh <robin.hester@spektrix.com>
2023-12-13 11:19:31 +0000

    Inverted the if for endofset in the GetScore method

--- Tennis/Class1.cs
+++ Tennis/Class1.cs
@@ -23,18 +23,16 @@
 
         public string GetScore()
         {
+            var isEndOfSet = player1Score >= 4 || player2Score >= 4;
             var scoresAreEven= player1Score == player2Score;
+            
             if (scoresAreEven)
             {
                 return EvenScores();
             }
-
-            var isEndOfSet = player1Score >= 4 || player2Score >= 4;
-            if (isEndOfSet)
-            {
-                return EndOfSetScore();
-            }
-            return LabelForPoints(player1Score) + "-" + LabelForPoints(player2Score);
+            
+            if (!isEndOfSet) return LabelForPoints(player1Score) + "-" + LabelForPoints(player2Score);
+            return EndOfSetScore();
         }
 
         private static string LabelForPoints(int score)
