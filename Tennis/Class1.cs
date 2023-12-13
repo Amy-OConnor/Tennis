869dbd96d829e143c6c7998f3932c3b1b36e91d0
robinh <robin.hester@spektrix.com>
2023-12-13 10:58:28 +0000

    Replaced win for player2 with a return

--- Tennis/Class1.cs
+++ Tennis/Class1.cs
@@ -60,7 +60,7 @@
             if (player1Score == player2Score + 1) return "Advantage player1";
             else if (player2Score == player1Score + 1) return "Advantage player2";
             else if (player1Score == player2Score + 2) return "Win for player1";
-            else score = "Win for player2";
+            else return "Win for player2";
             return score;
         }
 
